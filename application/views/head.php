e5af027daca77e25a1f8e7cdca2af99c98a54457
jakkti12 <jakkti005@gmail.com>
2024-01-22 08:46:27 +0700

    Edit footer and head

--- application/views/head.php
+++ application/views/head.php
@@ -7,7 +7,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Mitr:wght@500;600&display=swap" rel="stylesheet">
      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-     <link rel="stylesheet" href="/assets/style//content.css">
-     <link rel="stylesheet" href="/assets/style//nav.css">
-     <link rel="stylesheet" href="/assets/style//footer.css">
+     <link rel="stylesheet" href="/assets/style/content.css">
+     <link rel="stylesheet" href="/assets/style/nav.css">
+     <link rel="stylesheet" href="/assets/style/footer.css">
 </head>
